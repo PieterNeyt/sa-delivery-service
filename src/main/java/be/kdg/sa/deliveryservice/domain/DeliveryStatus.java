d0b9d2dfca39948d44fc09c59f92ea38c7dee263
Hugo Dor <hugo.dor@student.kdg.be>
2025-09-18 16:38:39 +0200

    tekst bij de enum toegevoegd van status gerecht

--- src/main/java/be/kdg/sa/deliveryservice/domain/DeliveryStatus.java
+++ src/main/java/be/kdg/sa/deliveryservice/domain/DeliveryStatus.java
@@ -1,9 +1,19 @@
 package be.kdg.sa.deliveryservice.domain;
 
+
+import lombok.Getter;
+
+@Getter
 public enum DeliveryStatus {
-    AVAILABLE,
-    PENDING,
-    ACCEPTED,
-    IN_ROUTE,
-    DELIVERED
+    AVAILABLE("We zoeken een coerier."),
+    PENDING("We zoeken een coerier"),
+    ACCEPTED("We hebben een coerier gevonden, restaurant is bezig met voorbereiding van uw order."),
+    IN_ROUTE("De coerier is onderweg met je bestelling."),
+    DELIVERED("Je eten is er");
+
+    private final String text;
+
+    DeliveryStatus(String text) {
+        this.text = text;
+    }
 }
